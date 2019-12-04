b1915433be4f92e70c4933fd2ea4a9852a0a1523
SeungLee <ls0408@naver.com>
2019-12-04 23:32:34 +0900

    update chapter7 code

---
+++ petstore/src/components/Product.vue
@@ -0,0 +1,53 @@
+<template>
+    <div>
+        <my-header></my-header>
+        <h1>id {{ $route.params.id }} 입니다.</h1>
+        <div class="row">
+            <div class="col-md-5 col-md-offset-0">
+                <figure>
+                    <img :src="product.image" class="product">
+                </figure>
+            </div>
+            <div class="col-md-6 col-md-offset-0 description">
+                <h1>{{ product.title }}</h1>
+                <p v-html="product.description"></p>
+                <p class="price">{{ product.price }}</p>
+                <button @click="edit">상품 수정</button>
+                <router-view></router-view>
+            </div>
+        </div>
+    </div>    
+</template>
+
+<script>
+import MyHeader from './Header.vue';
+
+export default {
+    components: { MyHeader },
+    data() {
+        return {
+            product: ''
+        };
+    },
+    created () {
+        axios.get('/static/products.json')
+        .then(response => {
+            this.product = response.data.products.filter(data => {
+                return data.id === this.$route.params.id;
+            })[0];
+            console.info(this.product.image);
+            
+            this.product.image = '/' + this.product.image;
+        })
+    },
+    methods: {
+        edit () {
+            this.$router.push({name: 'Edit'});
+        }
+    }    
+};
+</script>
+
+<style>
+    
+</style>
